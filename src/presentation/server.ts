409ea4fe1af5b17a4049b78e41231cdf5cf81a8d
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-10 19:38:44 -0500

    ✨create class check-servise and implement in server.ts

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./tasks/cron-service";
 
 export class Server {
@@ -9,8 +10,9 @@ export class Server {
         CronService.createJob(
             '*/5 * * * * *',
             () => {
-                const date = new Date();
-                console.log('5 seconds', date)
+
+                new CheckService().execute( 'https://google.com' )
+
             }
         );
 
