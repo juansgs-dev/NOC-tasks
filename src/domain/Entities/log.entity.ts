b95613dca31bb250ccf4e3d08d9a9c237ad87c35
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-11 22:35:29 -0500

    ✨new Entity log.Entity

---
+++ src/domain/Entities/log.entity.ts
@@ -0,0 +1,22 @@
+
+export enum LogSeverityLevel {
+    low = 'low',
+    medium = 'medium',
+    high = 'high'
+
+}
+
+export class LogEntity {
+
+    public level: string;
+    public message: string;
+    public createdAt: Date;
+
+    constructor(message: string, level: LogSeverityLevel) {
+        this.level = level;
+        this.message = message;
+        this.createdAt = new Date();
+        }
+
+
+}
