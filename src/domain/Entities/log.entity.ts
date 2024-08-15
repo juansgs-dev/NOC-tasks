8431110935e1aa5cfaf9269ebb7e8640c7bcdab8
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-14 22:21:16 -0500

    ✨create function fromJson

--- src/domain/Entities/log.entity.ts
+++ src/domain/Entities/log.entity.ts
@@ -16,7 +16,16 @@ export class LogEntity {
         this.level = level;
         this.message = message;
         this.createdAt = new Date();
-        }
+    }
+
+    static fromJson = ( json: string ): LogEntity => {
+        const { message, level, createdAt } = JSON.parse(json);
+        
+        const log = new LogEntity(message, level);
+        log.createdAt = new Date(createdAt);
+
+        return log;
+    }
 
 
 }
