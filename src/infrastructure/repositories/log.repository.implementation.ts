9b38cd82e478d2e58afe7f42b7a5e2fc6fba69f0
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-20 21:22:41 -0500

    ✨new LogRepositoryImplementation for implementation logDatasource

---
+++ src/infrastructure/repositories/log.repository.implementation.ts
@@ -0,0 +1,19 @@
+import { LogRepository } from "../../domain/repository/log.repository";
+import { LogDatasource } from '../../domain/datasources/log.datasources';
+import { LogEntity, LogSeverityLevel } from "../../domain/Entities/log.entity";
+
+
+export class LogRepositoryImplementation implements LogRepository {
+
+    constructor(
+        private readonly logDatasource: LogDatasource
+    ){}
+
+    async saveLog(log: LogEntity): Promise<void> {
+        return this.logDatasource.saveLog(log);
+    }
+
+    async getLogs(severitylevel: LogSeverityLevel): Promise<LogEntity[]> {
+        return this.logDatasource.getLogs(severitylevel);
+    }
+}
