4eeddb4ead8e875303dd096ea1e9e82b5d86bbb4
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-20 21:43:02 -0500

    ♻️ add property LogRepository and implementation in file

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from '../../Entities/log.entity';
+import { LogRepository } from "../../repository/log.repository";
+
 interface CheckServiceUseCase {
     execute( url: string ): Promise<boolean>;
 }
@@ -8,6 +11,7 @@ type ErrorCallback = ( error: string ) => void;
 export class CheckService implements CheckServiceUseCase{
 
     constructor(
+        private readonly logRepository: LogRepository,
         private readonly successCallback: SuccessCallback,
         private readonly errorCallback: ErrorCallback
     ){};
@@ -19,13 +23,17 @@ export class CheckService implements CheckServiceUseCase{
             if(!req.ok){
                 throw new Error(`Error on check service ${url}`);
             }
-
+            
+            const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low );
+            this.logRepository.saveLog(log);
             this.successCallback();
-
             return true;
 
         } catch (error) {
 
+            const errorMessage = `${error}`;
+            const log = new LogEntity(errorMessage, LogSeverityLevel.low )
+            this.logRepository.saveLog(log);
             this.errorCallback(`${error}`);
             return false;
         }
