db1bd40bac27c7a839c195f3e4625fdfefece496
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-22 23:10:57 -0500

    ♻️ refactor code for implementation of new params Origin

--- src/domain/Entities/log.entity.ts
+++ src/domain/Entities/log.entity.ts
@@ -3,7 +3,13 @@ export enum LogSeverityLevel {
     low = 'low',
     medium = 'medium',
     high = 'high'
+}
 
+export interface LogEntityOptions {
+    level: LogSeverityLevel;
+    message: string;
+    origin: string; 
+    createdAt?: Date;
 }
 
 export class LogEntity {
@@ -11,18 +17,26 @@ export class LogEntity {
     public level: string;
     public message: string;
     public createdAt: Date;
+    public origin: string; 
 
-    constructor(message: string, level: LogSeverityLevel) {
+    constructor( options: LogEntityOptions) {
+        const { level, message, origin, createdAt = new Date() } = options;
         this.level = level;
         this.message = message;
-        this.createdAt = new Date();
+        this.origin = origin;
+        this.createdAt = createdAt;
     }
 
     static fromJson = ( json: string ): LogEntity => {
-        const { message, level, createdAt } = JSON.parse(json);
+        const { message, level, origin, createdAt } = JSON.parse(json);
         
-        const log = new LogEntity(message, level);
-        log.createdAt = new Date(createdAt);
+        const log = new LogEntity({
+            message,
+            level, 
+            origin,
+            createdAt
+        });
+
 
         return log;
     }
