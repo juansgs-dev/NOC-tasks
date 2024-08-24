d886c728bd35ea599d2b8b93973e02b2ba148e51
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-23 23:33:12 -0500

    🚧 test functions sendEmail in server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImplementation } from "../infrastructure/repositories/log.repository.implementation";
 import { CronService } from "./tasks/cron-service";
+import { EmailService } from './email/email-service';
 
 const fileSystemLogRepository = new LogRepositoryImplementation(
     new FileSystemDatasource()
@@ -12,6 +13,17 @@ export class Server {
     public static start() {
 
         console.log('Server started...');
+
+        const emailService = new EmailService();
+
+        emailService.sendEmail({
+            to: 'juan_gonzalez82181@elpoli.edu.co',
+            subject: 'Prueba logs system',
+            htmlBody: `
+            <h3>Logs Sistema</h3>
+            <p>Ver logs</p>
+            `
+        });
 
         // CronService.createJob(
         //     '*/5 * * * * *',
