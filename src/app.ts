b2aa6000c22116a690d05f5402374f7823c73b42
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-25 22:31:11 -0500

    🔐 update .env file with Mongo variables

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import { envs } from "./config/plugin/envs.plugin";
 import { Server } from "./presentation/server";
 
 (async() => {
@@ -7,5 +6,4 @@ import { Server } from "./presentation/server";
 
 function main() {
     Server.start();
-    // console.log( envs.MAILER_EMAIL )
 }
