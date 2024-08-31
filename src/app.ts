50abe53e8a468b3e91dc08e446ee45fe97b5c2ef
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-08-30 23:00:08 -0500

    🚧 test connection in mongo db

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import { Server } from "./presentation/server";
+import { envs } from './config/plugin/envs.plugin';
+import { MongoDatabase } from './data/mongoConfig';
 
 (async() => {
     main();
 })();
 
-function main() {
-    Server.start();
+async function main() {
+
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME,
+      });
+    
+
+    // Server.start();
 }
