a94ee757111d4a91b76a12493847254918bcd168
JuanSe <juan_gonzalez82181@elpoli.edu.co>
2024-09-18 07:56:58 -0500

    🚧 testing save logs with conection to mongo DB

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -4,9 +4,11 @@ import { LogRepositoryImplementation } from "../infrastructure/repositories/log.
 import { CronService } from "./tasks/cron-service";
 import { EmailService } from './email/email-service';
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
+import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
 
-const fileSystemLogRepository = new LogRepositoryImplementation(
-    new FileSystemDatasource()
+const LogRepository = new LogRepositoryImplementation(
+    // new FileSystemDatasource(),
+    new MongoLogDatasource()
 );
 
 const emailService = new EmailService();
@@ -17,28 +19,28 @@ export class Server {
 
         console.log('Server started...');
 
-        const sendEmail = new SendEmailLogs(
-          emailService, 
-          fileSystemLogRepository,
-        ).execute(
-          ['sebassossa02@gmail.com','juan_gonzalez82181@elpoli.edu.co']
-        )
-        emailService.sendEmailWithFileSystemLogs(
-            ['sebassossa02@gmail.com','juan_gonzalez82181@elpoli.edu.co']
-        );
-
-        // CronService.createJob(
-        //     '*/5 * * * * *',
-        //     () => {
-        //         const url = 'https://google.com'; 
-        //         new CheckService(
-        //             fileSystemLogRepository,
-        //             () => console.log(`this ${url} is ok`),
-        //             ( error ) => console.log(error)
-        //         ).execute(url)
-
-        //     }
+        // const sendEmail = new SendEmailLogs(
+        //   emailService, 
+        //   fileSystemLogRepository,
+        // ).execute(
+        //   ['sebassossa02@gmail.com','juan_gonzalez82181@elpoli.edu.co']
+        // )
+        // emailService.sendEmailWithFileSystemLogs(
+        //     ['sebassossa02@gmail.com','juan_gonzalez82181@elpoli.edu.co']
         // );
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url = 'https://google.com'; 
+                new CheckService(
+                  LogRepository,
+                    () => console.log(`this ${url} is ok`),
+                    ( error ) => console.log(error)
+                ).execute(url)
+
+            }
+        );
 
     }
 
